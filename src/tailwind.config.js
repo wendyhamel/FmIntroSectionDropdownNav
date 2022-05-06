08696e05d79eda2c013d5f7aa40f25b45f1fbcea
wendyhamel <whamel@it4web.nl>
2022-05-06 13:12:59 +0200

    Start basic styling

---
+++ src/tailwind.config.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+    theme: {
+        fontFamily : {
+            'sans': ['Epilogue', 'sans-serif'],
+        },
+        extend: {
+            colors: {
+                'gray-light': 'hsl(0, 0%, 98%)',
+                'gray-medium': 'hsl(0, 0%, 41%)',
+                'gray-dark' : 'hsl(0, 0%, 8%)',
+            }
+        }
+    }
+}
